5358e9d7cef2b209a1d6738c9a1adbfd1a10b810
Jon Rickard Yxelflod <rickard@yxelflod.se>
2025-09-22 15:01:18 +0200

    Converted to main type program. Added class 'Die'

--- Program.cs
+++ Program.cs
@@ -1,78 +1,82 @@
 ﻿using System.Threading.Tasks.Sources;
 
-int score = 0;
-int atempts = 0;
-
-bool isGameOver = false;
-static int RollDice()
+internal class Program
 {
-    return new Random().Next(6) + 1;
-}
-
-static bool CheckAnswer(int answer, int product)
-{
-    return answer == product;
-}
-
-void Continue()
-{
-    Console.WriteLine("Tryck på mellanslag för att fortsätta.");
-
-    while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
-
-    atempts++;
-}
-
-
-do
-{
-    Console.Clear();
-
-    Console.WriteLine("Svara 'stopp' för att avsluta.\n");
-
-    string input;
-
-    int num1 = RollDice();
-    int num2 = RollDice();
-
-    int product = num1 * num2;
-
-    Console.WriteLine($"Vad blir {num1} * {num2}?");
-    Console.Write("\nSvar: ");
-    input = Console.ReadLine().Trim();
-
-    if (input == "stopp") { isGameOver = true; }
-
-    bool isNumber = int.TryParse(input, out int answer);
-
-    if (isNumber)
+    private static void Main(string[] args)
     {
-        if (CheckAnswer(answer, product))
+        int score = 0;
+        int atempts = 0;
+
+        bool isGameOver = false;
+        static int RollDice()
         {
-            Console.WriteLine("\nKorrekt!\n");
-            score++;
-        }
-        else
-        {
-            Console.WriteLine("\nDet var tyvärr inte rätt svar.\n");
+            return new Random().Next(6) + 1;
         }
 
-        Continue();
+        static bool CheckAnswer(int answer, int product)
+        {
+            return answer == product;
+        }
+
+        void Continue()
+        {
+            Console.WriteLine("Tryck på mellanslag för att fortsätta.");
+
+            while (Console.ReadKey().Key != ConsoleKey.Spacebar) { }
+
+            atempts++;
+        }
+
+
+        do
+        {
+            Console.Clear();
+
+            Console.WriteLine("Svara 'stopp' för att avsluta.\n");
+
+            string input;
+
+            int num1 = RollDice();
+            int num2 = RollDice();
+
+            int product = num1 * num2;
+
+            Console.WriteLine($"Vad blir {num1} * {num2}?");
+            Console.Write("\nSvar: ");
+            input = Console.ReadLine().Trim();
+
+            if (input == "stopp") { isGameOver = true; }
+
+            bool isNumber = int.TryParse(input, out int answer);
+
+            if (isNumber)
+            {
+                if (CheckAnswer(answer, product))
+                {
+                    Console.WriteLine("\nKorrekt!\n");
+                    score++;
+                }
+                else
+                {
+                    Console.WriteLine("\nDet var tyvärr inte rätt svar.\n");
+                }
+
+                Continue();
+            }
+            else if (input == "stopp")
+            {
+                isGameOver = true;
+            }
+            else
+            {
+                Console.WriteLine("\nSkrev du verkligen in ett nummer?\n");
+
+                Continue();
+            }
+        } while (!isGameOver);
+
+        Console.WriteLine($"\nDu fick {score} poäng på {atempts} försök.");
+
+        Console.WriteLine("\nTack för att du spelade!");
     }
-    else if (input == "stopp")
-    {
-        isGameOver = true;
-    }
-    else
-    {
-        Console.WriteLine("\nSkrev du verkligen in ett nummer?\n");
-
-        Continue();
-    }
-} while (!isGameOver);
-
-Console.WriteLine($"\nDu fick {score} poäng på {atempts} försök.");
-
-Console.WriteLine("\nTack för att du spelade!");
-
-Console.ReadKey();
+}
